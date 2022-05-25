9460dc407be67e1733e114879fdb5e4ed2311d60
David Puerto Guerrero <dsp5502@gmail.com>
2022-05-25 10:45:32 -0500

    firebase Async actions

--- src/redux/actions/actionCocktails.js
+++ src/redux/actions/actionCocktails.js
@@ -1,3 +1,13 @@
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from '../../Firebase/fireBaseConfing';
 import { typesCocktail } from '../types/types';
 
 export const listCocktail = (cocktail) => ({
@@ -5,12 +15,47 @@ export const listCocktail = (cocktail) => ({
   payload: cocktail,
 });
 
+export const listProductAsync = () => {
+  return async (dispatch) => {
+    const collectionTraer = await getDocs(collection(db, 'productpsAmazonas'));
+    const productos = [];
+    collectionTraer.forEach((doc) => {
+      productos.push({ ...doc.data() });
+    });
+    dispatch(listCocktail(productos));
+  };
+};
+
 export const addCocktail = (cocktail) => ({
   type: typesCocktail.addCocktail,
   payload: cocktail,
 });
+export const addProductAsync = (cocktail) => {
+  return (dispatch) => {
+    addDoc(collection(db, 'cocktails'), cocktail)
+      .then((resp) => {
+        dispatch(addCocktail(cocktail));
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  };
+};
 
 export const DeleteCocktail = (id) => ({
   type: typesCocktail.deleteCocktail,
   payload: id,
 });
+
+export const deleteProductAsync = (id) => {
+  return async (dispatch) => {
+    const colleccionTraer = collection(db, 'cocktails');
+    const q = query(colleccionTraer, where('id', '==', id));
+    const traerDatosQ = await getDocs(q);
+    traerDatosQ.forEach((docum) => {
+      deleteDoc(doc(db, 'productpsAmazonas', docum.id));
+    });
+    dispatch(DeleteCocktail(id));
+    dispatch(listProductAsync());
+  };
+};
