f9a030249a43183469c0d60728dd829c59cd2eda
David Puerto Guerrero <dsp5502@gmail.com>
2022-05-25 11:55:26 -0500

    change

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -3,20 +3,20 @@ import Search from '../components/search/Search';
 import { getData } from '../helpers/getData';
 
 function AppRoutes() {
-
   const urlCocktails =
     'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Brandy';
 
   getData(urlCocktails).then(console.log).catch(console.log);
 
-
-
-  return <div className='App'>
-    <center> <Search /></center>
-    <Cards></Cards>
-
-  </div>;
-
+  return (
+    <div className='App'>
+      <center>
+        {' '}
+        <Search />
+      </center>
+      <Cards></Cards>
+    </div>
+  );
 }
 
 export default AppRoutes;
