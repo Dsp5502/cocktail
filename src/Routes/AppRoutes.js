bff536e4ca5f336080b92e3f7033a2492ef769d4
Daniel Corredor <dann95.moz@gmail.com>
2022-05-25 11:11:42 -0500

    usestate

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import Cards from '../components/cards/Cards';
 import Search from '../components/search/Search';
 import { getData } from '../helpers/getData';
 
 function AppRoutes() {
+  const [cocktel, setCocktel] = useState('')
 
   const urlCocktails =
-    'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Brandy';
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktel}`;
 
   getData(urlCocktails).then(console.log).catch(console.log);
 
