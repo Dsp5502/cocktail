281e2d13ed6869afc943fbd749a67c53ecabcdea
Daniel Corredor <dann95.moz@gmail.com>
2022-05-25 10:23:03 -0500

    card

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,5 +1,20 @@
 function AppRoutes() {
-  return <div className='App'>hola</div>;
+
+
+  return <div className='App'>
+
+    <div className='card'>
+      <div>
+        <img src="" alt="cocktel" className="cardImg" />
+      </div>
+
+      <div className='BottomCardInfo'>
+        <button>agregar</button>
+        <button>eliminar</button>
+      </div>
+    </div>
+
+  </div>;
 }
 
 export default AppRoutes;
