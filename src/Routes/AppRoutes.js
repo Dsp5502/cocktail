7e446aef8c9a6e65dd9daa755773853673fde56f
Daniel Corredor <dann95.moz@gmail.com>
2022-05-25 11:50:04 -0500

    landing

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Cards from '../components/cards/Cards';
 import Search from '../components/search/Search';
 import { getData } from '../helpers/getData';
 
 function AppRoutes() {
   const [cocktel, setCocktel] = useState('')
-
+  const [infoCocktel, setInfoCocktel] = useState([])
   const urlCocktails =
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktel}`;
+  useEffect(() => {
+    getData(urlCocktails).then(
+      cocktel => {
+        setInfoCocktel(cocktel)
+      }
+    ).catch(console.log);
+  }, [])
 
-  getData(urlCocktails).then(console.log).catch(console.log);
+  console.log(infoCocktel, "test");
 
 
 
   return <div className='App'>
     <center> <Search /></center>
-    <Cards></Cards>
+    <Cards infoCocktel={infoCocktel} />
 
   </div>;
 
